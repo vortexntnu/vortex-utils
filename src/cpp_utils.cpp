d7b5bf8474741f72fe38a6c3c7292f3432cede9b
Anders Høgden <andeshog@stud.ntnu.no>
2025-02-14 20:07:46 +0100

    add some more functionality to Eta struct

--- src/cpp_utils.cpp
+++ src/cpp_utils.cpp
@@ -1,7 +1,21 @@
 #include "vortex_utils/cpp_utils.hpp"
 
-double vortex_utils::ssa(const double angle)
-{
+namespace vortex_utils {
+double ssa(const double angle) {
     double angle_ssa = fmod(angle + M_PI, 2 * M_PI) - M_PI;
     return angle_ssa;
 }
+
+Matrix3d skew_symmetric(const Eigen::Vector3d& vector) {
+    Matrix3d skew_symmetric_matrix;
+    skew_symmetric_matrix << 0, -vector.z(), vector.y(), vector.z(), 0,
+        -vector.x(), -vector.y(), vector.x(), 0;
+    return skew_symmetric_matrix;
+}
+
+Eigen::Vector3d quat_to_euler(const Eigen::Quaterniond& q) {
+    Eigen::Vector3d euler_angles = q.toRotationMatrix().eulerAngles(0, 1, 2);
+    return euler_angles;
+}
+
+}  // namespace vortex_utils
