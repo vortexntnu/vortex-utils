7871d82b34bc4eefabbc039a4a2675dc4da4ccc8
Anders Høgden <andeshog@stud.ntnu.no>
2025-02-25 15:15:59 +0100

    docs

--- test/test_cpp_utils.cpp
+++ test/test_cpp_utils.cpp
@@ -3,22 +3,27 @@
 
 namespace vortex_utils {
 
+// Test that the value does not change when already in the interval [-pi, pi]
 TEST(ssa, test_ssa_0) {
     EXPECT_EQ(0, ssa(0));
 }
 
+// Test that 2 pi correctly maps to 0
 TEST(ssa, test_ssa_2pi) {
     EXPECT_EQ(0, ssa(2 * M_PI));
 }
 
+// Test that values over pi gets mapped to the negative interval
 TEST(ssa, test_ssa_3_5) {
     EXPECT_NEAR(-2.78, ssa(3.5), 0.01);
 }
 
+// Test that values under -pi gets mapped to the positive interval
 TEST(ssa, test_ssa_minus_3_5) {
     EXPECT_NEAR(2.78, ssa(-3.5), 0.01);
 }
 
+// Test that the skew-symmetric matrix is correctly calculated
 TEST(skew_symmetric, test_skew_symmetric) {
     Eigen::Vector3d vector(1, 2, 3);
     Eigen::Matrix3d expected;
@@ -28,6 +33,7 @@ TEST(skew_symmetric, test_skew_symmetric) {
     EXPECT_EQ(expected, result);
 }
 
+// Test that the identity quaternion correctly maps to 0, 0, 0
 TEST(quat_to_euler, test_quat_to_euler_1) {
     Eigen::Quaterniond q(1.0, 0.0, 0.0, 0.0);
     Eigen::Vector3d expected(0.0, 0.0, 0.0);
@@ -37,6 +43,7 @@ TEST(quat_to_euler, test_quat_to_euler_1) {
     }
 }
 
+// Test that only changing w and x in the quat only affects roll
 TEST(quat_to_euler, test_quat_to_euler_2) {
     Eigen::Quaterniond q2(0.707, 0.707, 0.0, 0.0);
     Eigen::Vector3d expected2(1.57, 0.0, 0.0);
@@ -46,6 +53,7 @@ TEST(quat_to_euler, test_quat_to_euler_2) {
     }
 }
 
+// Test that only changing w and y in the quat only affects pitch
 TEST(quat_to_euler, test_quat_to_euler_3) {
     Eigen::Quaterniond q3(0.707, 0.0, 0.707, 0.0);
     Eigen::Vector3d expected3(0.0, 1.57, 0.0);
@@ -55,6 +63,7 @@ TEST(quat_to_euler, test_quat_to_euler_3) {
     }
 }
 
+// Test that only changing w and z in the quat only affects yaw
 TEST(quat_to_euler, test_quat_to_euler_4) {
     Eigen::Quaterniond q4(0.707, 0.0, 0.0, 0.707);
     Eigen::Vector3d expected4(0.0, 0.0, 1.57);
