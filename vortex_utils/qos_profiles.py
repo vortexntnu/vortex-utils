49f15d3de6c48c8c1800ca0b0107a13432f9bb78
Anders Høgden <anders.hogden@zeabuz.com>
2025-10-10 22:06:44 +0200

    feat: add python qos profiles

---
+++ vortex_utils/qos_profiles.py
@@ -0,0 +1,14 @@
+from rclpy import qos
+
+def sensor_data_profile(depth: int = 5) -> qos.QoSProfile:
+    return qos.QoSProfile(
+        history=qos.HistoryPolicy.KEEP_LAST,
+        depth=depth,
+        reliability=qos.ReliabilityPolicy.BEST_EFFORT
+    )
+
+def reliable_profile(depth: int = 10) -> qos.QoSProfile:
+    return qos.QoSProfile(
+        history=qos.HistoryPolicy.KEEP_LAST,
+        depth=depth
+    )
