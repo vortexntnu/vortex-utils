04e4dae71e6fbcbb2ba4e5da5fb92bd7c2a0b1c4
Jorgen Fjermedal <jorgen.fjermedal@hotmail.com>
2025-12-10 11:00:48 +0100

    reference-to-pose documentation

--- include/vortex/utils/ros_conversions.hpp
+++ include/vortex/utils/ros_conversions.hpp
@@ -6,6 +6,17 @@
 
 namespace vortex::utils::ros_conversions {
 
+/**
+ * @brief Concept describing a generic pose-like type.
+ *
+ * A type satisfies this concept if it exposes the following fields,
+ * all convertible to double:
+ * - x, y, z  (position components)
+ * - roll, pitch, yaw  (orientation expressed as Euler angles)
+ *
+ *
+ * @tparam T The candidate type to check.
+ */
 template <typename T>
 concept PoseLike = requires(const T& t) {
     { t.x } -> std::convertible_to<double>;
@@ -16,6 +27,20 @@ concept PoseLike = requires(const T& t) {
     { t.yaw } -> std::convertible_to<double>;
 };
 
+/**
+ * @brief Convert a pose-like reference structure into a ROS Pose message.
+ *
+ * The function reads position (x, y, z) and orientation (roll, pitch, yaw)
+ * from the input object and constructs a corresponding
+ * `geometry_msgs::msg::Pose`.
+ *
+ * Orientation is internally converted from Euler angles (roll, pitch, yaw)
+ * into a quaternion via `vortex::utils::math::euler_to_quat()`.
+ *
+ * @tparam T A type satisfying the PoseLike concept.
+ * @param ref The input pose-like object.
+ * @return A `geometry_msgs::msg::Pose` containing the converted pose.
+ */
 template <PoseLike T>
 geometry_msgs::msg::Pose reference_to_pose(const T& ref) {
     geometry_msgs::msg::Pose pose;
