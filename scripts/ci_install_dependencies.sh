9b3a3e3152c832da7e8953a96e063ff84649f30a
Jorgen Fjermedal <jorgen.fjermedal@hotmail.com>
2026-01-14 19:13:43 +0100

    Add ubuntu PPA for gcc 13

--- scripts/ci_install_dependencies.sh
+++ scripts/ci_install_dependencies.sh
@@ -23,6 +23,12 @@ echo "Installing PyGObject dependencies via apt..."
 sudo apt install -y libglib2.0-dev libcairo2-dev libgirepository1.0-dev \
     gir1.2-gtk-3.0 python3-gi-cairo
 
+# Add Ubuntu Toolchain PPA to get gcc-13/g++-13
+sudo apt-get update -qq
+sudo apt-get install -y --no-install-recommends software-properties-common
+sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
+sudo apt-get update -qq
+
 # Install and switch to GCC 13
 sudo apt-get install -y --no-install-recommends gcc-13 g++-13 lcov
 sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-13 100
