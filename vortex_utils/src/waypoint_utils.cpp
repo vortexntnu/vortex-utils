edb3460894a0849e167de0ebcc8b96e433d9a48e
jorgenfj <144696109+jorgenfj@users.noreply.github.com>
2026-03-24 18:49:20 +0100

    waypoint types, utils and ros conversions (#46)
    * waypoint types, utils and ros conversions
    
    * update to support separate mode msg

---
+++ vortex_utils/src/waypoint_utils.cpp
@@ -0,0 +1,110 @@
+#include "vortex/utils/waypoint_utils.hpp"
+
+#include <yaml-cpp/yaml.h>
+#include <cmath>
+#include <vortex/utils/math.hpp>
+
+namespace vortex::utils::waypoints {
+
+Pose compute_waypoint_goal(const Pose& incoming_waypoint,
+                           WaypointMode mode,
+                           const Pose& current_state) {
+    Pose waypoint_out = incoming_waypoint;
+
+    switch (mode) {
+        case WaypointMode::FULL_POSE:
+            break;
+
+        case WaypointMode::ONLY_POSITION:
+            waypoint_out.set_ori(current_state.ori_quaternion());
+            break;
+
+        case WaypointMode::FORWARD_HEADING: {
+            double dx = incoming_waypoint.x - current_state.x;
+            double dy = incoming_waypoint.y - current_state.y;
+            double forward_heading = std::atan2(dy, dx);
+
+            waypoint_out.set_ori(Eigen::Quaterniond(
+                Eigen::AngleAxisd(forward_heading, Eigen::Vector3d::UnitZ())));
+            break;
+        }
+
+        case WaypointMode::ONLY_ORIENTATION:
+            waypoint_out.set_pos(current_state.pos_vector());
+            break;
+    }
+
+    return waypoint_out;
+}
+
+bool has_converged(const Pose& state,
+                   const Pose& waypoint_goal,
+                   WaypointMode mode,
+                   double convergence_threshold) {
+    const Eigen::Vector3d ep = state.pos_vector() - waypoint_goal.pos_vector();
+
+    const Eigen::Vector3d ea = vortex::utils::math::quaternion_error(
+        state.ori_quaternion(), waypoint_goal.ori_quaternion());
+
+    const double err = [&] {
+        switch (mode) {
+            case WaypointMode::ONLY_POSITION:
+                return ep.norm();
+            case WaypointMode::ONLY_ORIENTATION:
+                return ea.norm();
+            case WaypointMode::FORWARD_HEADING:
+                return std::sqrt(ep.squaredNorm() + ea(2) * ea(2));
+            case WaypointMode::FULL_POSE:
+            default:
+                return std::sqrt(ep.squaredNorm() + ea.squaredNorm());
+        }
+    }();
+
+    return err < convergence_threshold;
+}
+
+Pose apply_pose_offset(const Pose& base, const Pose& offset) {
+    const Eigen::Vector3d p_base = base.pos_vector();
+    const Eigen::Quaterniond q_base = base.ori_quaternion().normalized();
+
+    const Eigen::Vector3d p_offset = offset.pos_vector();
+    const Eigen::Quaterniond q_offset = offset.ori_quaternion().normalized();
+
+    const Eigen::Vector3d p_target = p_base + p_offset;
+    const Eigen::Quaterniond q_target = (q_base * q_offset).normalized();
+
+    return Pose::from_eigen(p_target, q_target);
+}
+
+Pose load_waypoint_from_yaml(const std::string& file_path,
+                             const std::string& identifier) {
+    YAML::Node root = YAML::LoadFile(file_path);
+
+    if (!root[identifier]) {
+        throw std::runtime_error("Waypoint '" + identifier + "' not found in " +
+                                 file_path);
+    }
+
+    const auto& wp = root[identifier];
+
+    const double x = wp["position"]["x"].as<double>();
+    const double y = wp["position"]["y"].as<double>();
+    const double z = wp["position"]["z"].as<double>();
+
+    const double roll = wp["orientation"]["roll"].as<double>();
+    const double pitch = wp["orientation"]["pitch"].as<double>();
+    const double yaw = wp["orientation"]["yaw"].as<double>();
+
+    const Eigen::Quaterniond q =
+        vortex::utils::math::euler_to_quat(roll, pitch, yaw);
+
+    return Pose{.x = x,
+                .y = y,
+                .z = z,
+                .qw = q.w(),
+                .qx = q.x(),
+                .qy = q.y(),
+                .qz = q.z()};
+}
+
+}  // namespace vortex::utils::waypoints
---
+++ vortex_utils_ros/include/vortex/utils/ros/waypoint_ros_conversions.hpp
@@ -0,0 +1,47 @@
+#ifndef VORTEX_UTILS_ROS__WAYPOINT_ROS_CONVERSIONS_HPP_
+#define VORTEX_UTILS_ROS__WAYPOINT_ROS_CONVERSIONS_HPP_
+
+#include <geometry_msgs/msg/pose.hpp>
+#include <vortex/utils/ros/ros_conversions.hpp>
+#include <vortex/utils/types.hpp>
+#include <vortex/utils/waypoint_utils.hpp>
+#include <vortex_msgs/msg/waypoint.hpp>
+#include <vortex_msgs/msg/waypoint_mode.hpp>
+
+namespace vortex::utils::waypoints {
+
+/**
+ * @brief Convert a ROS waypoint mode to a WaypointMode enum.
+ * @throws std::invalid_argument if the mode value is not recognized.
+ */
+inline WaypointMode waypoint_mode_from_ros(
+    const vortex_msgs::msg::WaypointMode& mode_msg) {
+    switch (mode_msg.mode) {
+        case vortex_msgs::msg::WaypointMode::FULL_POSE:
+            return WaypointMode::FULL_POSE;
+        case vortex_msgs::msg::WaypointMode::ONLY_POSITION:
+            return WaypointMode::ONLY_POSITION;
+        case vortex_msgs::msg::WaypointMode::FORWARD_HEADING:
+            return WaypointMode::FORWARD_HEADING;
+        case vortex_msgs::msg::WaypointMode::ONLY_ORIENTATION:
+            return WaypointMode::ONLY_ORIENTATION;
+        default:
+            throw std::invalid_argument("Invalid ROS waypoint mode: " +
+                                        std::to_string(mode_msg.mode));
+    }
+}
+
+/**
+ * @brief Convert a ROS Waypoint message to an internal Waypoint struct.
+ */
+inline vortex::utils::types::Waypoint waypoint_from_ros(
+    const vortex_msgs::msg::Waypoint& ros_wp) {
+    vortex::utils::types::Waypoint wp;
+    wp.pose = vortex::utils::ros_conversions::ros_pose_to_pose(ros_wp.pose);
+    wp.mode = waypoint_mode_from_ros(ros_wp.waypoint_mode);
+    return wp;
+}
+
+}  // namespace vortex::utils::waypoints
+
+#endif  // VORTEX_UTILS_ROS__WAYPOINT_ROS_CONVERSIONS_HPP_
